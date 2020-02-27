37e7b7594cf325a698c5d1201a56ca6b945e738f
JohannaP <55647359+JohPak@users.noreply.github.com>
2020-02-27 23:16:07 +0200

    commit

---
+++ textwriter.php
@@ -0,0 +1,21 @@
+<?php
+
+// lähde: https://www.dreamincode.net/forums/topic/69055-how-to-save-a-txt-file-to-a-server-using-php-code/
+
+// Open the file in write mode
+$handle = fopen("highscore.txt","w+");
+// If successful
+if ($handle && isset($_POST["highscore"])) {
+    $highscore = explode(",", $_POST["highscore"]);
+    // Write to that handle the username submitted in the form and the date
+    fwrite($handle, $highscore[0] . "\n" . $highscore[1]);
+    // Close the file
+    fclose($handle);
+
+    echo "OK";
+}
+else
+{
+    echo "NOK";
+}
+?>
